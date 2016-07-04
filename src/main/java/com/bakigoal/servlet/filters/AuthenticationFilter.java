81312f288c8c2273c4fa0c27d87a81e572c51770
Ilmir Bakirov <ilmirbakirov@gmail.com>
2016-07-04 16:51:50 +0500

    Initial commit

---
+++ src/main/java/com/bakigoal/servlet/filters/AuthenticationFilter.java
@@ -0,0 +1,53 @@
+package com.bakigoal.servlet.filters;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by ilmir on 04.07.16.
+ */
+@WebFilter("/AuthenticationFilter")
+public class AuthenticationFilter implements Filter {
+
+  private Logger logger = Logger.getLogger(AuthenticationFilter.class);
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    logger.info("AuthenticationFilter initialized");
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest req = (HttpServletRequest) request;
+    HttpServletResponse res = (HttpServletResponse) response;
+
+    String uri = req.getRequestURI();
+    logger.info("Requested Resource::"+uri);
+
+    HttpSession session = req.getSession(false);
+
+    if(session == null && !(uri.endsWith("html") || uri.endsWith("Login") || uri.endsWith("Register"))){
+      logger.error("Unauthorized access request");
+      res.sendRedirect("login.html");
+    }else{
+      // pass the request along the filter chain
+      chain.doFilter(request, response);
+    }
+  }
+
+  @Override
+  public void destroy() {
+    //close any resources here
+  }
+}
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,17 @@
+<%@page import="com.bakigoal.util.User" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+<%User user = (User) session.getAttribute("User"); %>
+<h3>Hi <%=user.getName() %></h3>
+<strong>Your Email</strong>: <%=user.getEmail() %><br>
+<strong>Your Country</strong>: <%=user.getCountry() %><br>
+<br>
+<form action="Logout" method="post">
+    <input type="submit" value="Logout" >
+</form>
+</body>
+</html>
